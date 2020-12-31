fd9661b2f4ced37cf7abd96b10c1240ea189220e
Hernan <hernan.grecco@gmail.com>
2020-12-31 01:28:00 -0300

    Added NO_NUMBA variable to nbcompat

--- nbkode/nbcompat/nb_to_import.py
+++ nbkode/nbcompat/nb_to_import.py
@@ -19,9 +19,12 @@ if os.environ.get("NBKODE_NONUMBA", 0):
     def is_jitted(func):
         return True
 
+    NO_NUMBA = True
 
 else:
     import numba  # noqa: F401
     from numba.extending import is_jitted  # noqa: F401
 
     numba.jitclass = numba.experimental.jitclass
+
+    NO_NUMBA = False
